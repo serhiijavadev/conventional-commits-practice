97d76b000d8eedb10656e373c082338dc344d484
serhiijavadev <javasvlugovoycom@gmail.com>
2026-06-28 21:00:49 +0800

    chore: initialize spring boot project

---
+++ src/main/java/com/example/conventionalcommitspractice/ConventionalCommitsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.conventionalcommitspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConventionalCommitsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConventionalCommitsPracticeApplication.class, args);
+    }
+
+}
